e2b1b16385a13a24e1b65d565e412a0f6f28597b
Miguel Quintero <quinteromdavid@gmail.com>
2020-06-23 21:19:23 +0000

    Test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""#  Ejemplo 1 de Funciones
+#  Ejemplo 1 de Funciones
 def saludar():
   print("Hola")
 saludar()
@@ -41,59 +41,87 @@ def ejemplo3(*args,**kwargs):
 ejemplo3("Hola", esto = "es un mensaje")
 
 
-#  Ejemplo 8()
+#  Ejemplo 8
 
 datos= {}
 entrada = ""
 
-while(entrada != "parar"):
-  nombre = input("Cuál es tu nombre?: ")
-  if(nombre in datos):
-    print("Este usuario ya existe")
-  else:
+while(1<2):
+  entrada = input("Que deseas hacer?: ")
+
+  if (not entrada):
+      print ("Estos son los comandos: \nregistrarme\nparar\nborrar_usuario\nver_usuario")
+      continue # ya lo veía venir : v que hice mal como era que se hacía eso :thinking:
+
+  if(entrada == "registrarme"):
+      # solo te va a pedir si se cumple arriba
+    nombre = input("Cuál es tu nombre?: ")
+
+    if(nombre in datos):
+        print("Este usuario ya existe")
+        continue
+
     contraseña = input("Cuál es tu contraseña?: ")
     datos[nombre] = contraseña
-    entrada = input("Que deseas hacer?: ")
-  if(entrada == "parar"):
-    print("Hemos terminado")
+
+  elif(entrada == "parar"): # si no es registrar entonces es parar?
+    print("Hemos terminado") #muestra y para
+    break
+
   elif(entrada == "ver_usuario"):
-    print(datos)
+    print("Estos son los usuarios registrados", datos) 
+
   elif(entrada == "borrar_usuario"):
     borrar_usuario = input("Cuál quieres borrar?: ")
-    del datos[borrar_usuario]
-    print(datos)"""
-  
+
+    if(borrar_usuario not in datos):
+      print("Este usuario no existe")
+
+    elif(borrar_usuario in datos):
+      del datos[borrar_usuario]
+      print("El usuario ha sido borrado con éxito", datos)
+
+#entonces que pasa ya lo arreglaste o que 
+# deja aahi
 
 #  Ejemplo 9
 
-"""usuarios = {}
+usuarios = {} 
+entrada = ""
 
 def registrar(nombre, contraseña):
-  if (nombre not in usuarios):
-    usuario[nombre] = contraseña
-
-while (1<2):
-  usuario = input("nombre: ")
-  contraseña = input("Contraseña: ")
-
-  registrar(usuario, contraseña)
-
-usuarios = {}
-
-def registrar(nombre, contraseña):
-  if (nombre not in usuarios):
-    usuarios[nombre] = contraseña
-
-while (1<2):
-  nombre = input("Usuario: ")
-  contraseña = input("Contraseña: ")
-  registrar(nombre, contraseña)
-  print(usuarios)
-
-usuarios = {}
+  if (nombre not in usuarios): #si nombre no está en el diccionario
+    usuarios[nombre] = contraseña #acá le agregas el nombre que quieras
 def ver_usuarios():
-  
   print(usuarios)
-ver_usuarios()"""
 
-print("Hola GitHub")
+def borrar_usuario():
+  if(usuario in usuarios):
+    usuario_borrar = input("Cuál quieres borrar?: ")
+    del usuarios[usuario_borrar] #acá según me explicó don mau él dice que si yo 
+    print(usuarios)
+
+while (len(usuarios) < 2):
+  usuario = input("nombre: ") 
+  if(usuario in usuarios):
+    print("Este usuario ya existe")
+    continue
+  if (len(usuario) < 3): #Si len(<- este devuelve la cantidad de letras o items como dice mau) es menor a 3
+    print("tu nombre tiene que tener mas de 3 letras")
+    continue
+  elif(len(usuario) > 15): #entonces si len("") es mayor a 15
+    print("Tu nombre no puede tener más de 15 letras")
+    continue
+  contraseña = input("Contraseña: ")
+  entrada = input("Que hacemos?: ")
+
+  registrar(usuario, contraseña) #Recuerda el orden <- esto también
+  if(entrada == "parar"):
+    print("Hemos terminado")
+    break
+  if(entrada == "ver_usuarios"):
+      ver_usuarios() # asi ctmr : v si no pones () 
+  elif(entrada == "borrar_usuario"):
+      borrar_usuario()
+
+  print ("\n")
